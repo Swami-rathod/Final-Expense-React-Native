90d47d78567b03a8a900c2e62fd94805a53baf4f
Vivek Rajesh Rathod <Vivek@Nomans-MacBook-Pro.local>
2024-03-15 23:57:48 +0530

    final push

---
+++ screens/SignupScreen.js
@@ -0,0 +1,34 @@
+import { useContext, useState } from 'react';
+import { Alert } from 'react-native';
+
+import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { createUser } from '../util/auth';
+import { AuthContext } from '../store/auth-context';
+
+function SignupScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+    const AuthCtx = useContext(AuthContext);
+
+  async function signupHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+    const token =   await createUser(email, password);
+    AuthCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert(
+        'Authentication failed',
+        'Could not create user, please check your input and try again later.'
+      );
+    }
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />;
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
+}
+
+export default SignupScreen;
